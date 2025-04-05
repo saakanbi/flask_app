4e3e427faa07196bedf38f4fa74ed4d20634dde2
saakanbi <woakanbi@gmail.com>
2025-04-05 13:43:17 -0600

    Updating directory...

--- ansible/roles/setup_mongodb/tasks/files/init_db.js
+++
@@ -1,7 +0,0 @@
-use securedb;
-db.messages.insertMany([
-  { text: "Hello from MongoDB!" },
-  { text: "Secure DevOps app deployed with verified artifact!" }
-  { text: "This app uses HTTPS via Let's Encrypt" },
-  { text: "Deployed securely with Ansible and Jenkins" }
-]);
