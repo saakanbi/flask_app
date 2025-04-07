0f023f57a4fb1b199f5f85ff89670e68f1992c91
saakanbi <woakanbi@gmail.com>
2025-04-07 16:18:07 -0600

    final fix

--- ansible/roles/setup_mongodb/files/init_db.js
+++ ansible/roles/setup_mongodb/files/init_db.js
@@ -1,6 +1,9 @@
 use securedb;
+
 db.messages.insertMany([
-  { text: "Hello from MongoDB! This is team32 statis page"},
-  { text: "This app uses HTTPS via Let's Encrypt" },
-  { text: "Deployed securely with Ansible and Jenkins" }
+  { sender: "Team32", content: "Hello from MongoDB! This is team32 static page" },
+  { sender: "Team32", content: "This app uses HTTPS via Let's Encrypt" },
+  { sender: "Team32", content: "Deployed securely with Ansible and Jenkins" }
 ]);
+
+print("✅ Inserted messages with sender + content format.");
